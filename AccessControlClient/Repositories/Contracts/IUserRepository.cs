61b252eb32e6fd44a2ee30dbc5113faf5f0b167b
Gytis <Gyciauskas@hotmail.com>
2018-04-23 17:49:00 +0300

    Improved code quality

---
+++ AccessControlClient/Repositories/Contracts/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using AccessControlClient.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccessControlClient.Repositories.Contracts
+{
+    public interface IUserRepository
+    {
+        Task<IReadOnlyList<User>> GetAllUsersAsync();   
+    }
+}
---
+++ AccessControlClient/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using AccessControlClient.Repositories.Contracts;
+using AccessControlClient.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AccessControlClient.Repositories
+{
+    public class UserRepository //: IUserRepository
+    {
+        private readonly HttpClient _httpClient; 
+
+        public UserRepository(HttpClient client)
+        {
+            _httpClient = client;
+        }
+
+        //public async Task<IReadOnlyList<User>> GetAllUsersAsync()
+        //{
+        //    var response = await _httpClient."http://localhost:58009/api/users?skip=0&take=10000");
+
+        //    var userList = response.Content;
+
+
+        //}
+    }
+}
